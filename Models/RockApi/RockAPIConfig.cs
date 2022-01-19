bba54b4efc58c5d2e9959a2c9952c8e6ca5b3f58
Adam <asfmadx@gmail.com>
2022-01-19 15:45:46 -0500

    Fixes

--- Models/RockApi/RockAPIConfig.cs
+++ Models/RockApi/RockAPIConfig.cs
@@ -10,9 +10,9 @@ namespace RockApi
     public static class RockAPIConfig
     {
         public static string ApiBase = "https://api.referralrock.com";
-        public static string PublicKey = "";
-        public static string PrivateKey = "";
-        public static string AuthToken = "";
+        public static string PublicKey = "e6de812d-32ec-4366-b719-21fd15ab1e48";
+        public static string PrivateKey = "9b69c8a2-f95d-43f7-9512-fdeecbc5ffda";
+        public static string AuthToken = "ZTZkZTgxMmQtMzJlYy00MzY2LWI3MTktMjFmZDE1YWIxZTQ4OjliNjljOGEyLWY5NWQtNDNmNy05NTEyLWZkZWVjYmM1ZmZkYQ==";
 
         public static void SetKeys(string PublicKey, string PrivateKey, string Auth)
         {
